9143bb628efb550a4821fdae06b04f1eae84bb2f
知念英弥 <s19013@std.it-college.ac.jp>
2024-03-21 23:11:07 +0900

    生成したファイルをルーティング

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,22 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Index  from '@/views/Index.vue'
+import Create  from '@/views/Create.vue'
+import Edit  from '@/views/Edit.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL + 'task'),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'index',
+      component: Index
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/create',
+      name: 'create',
+      component: Create
+    },
+    {
+      path: '/edit',
+      name: 'edit',
+      component: Edit
+    },
   ]
 })
 
