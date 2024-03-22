ed004d45d74ef082d417c749b791969362c6ae79
知念英弥 <s19013@std.it-college.ac.jp>
2024-03-22 15:03:52 +0900

    ai曰くそのまま設定するよりcreateで設定することが推奨されているらしいので編集

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,18 +6,6 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-// axios
-import axios from 'axios';
-window.axios = axios;
-axios.defaults.withCredentials = true;
-window.axios.defaults.headers.common['UserLang'] = window.navigator.language;
-axios.defaults.headers.common['Content-Type'] = 'application/json;charset=utf-8';
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-
-axios.defaults.baseURL = 'http://localhost:8000/api/task/';
-
-
-
 const app = createApp(App)
 
 app.use(createPinia())
---
+++ frontend/src/tools/customizedAxios.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const customizedAxios = axios.create({
+  baseURL: 'http://localhost:8000/api/task/',
+  withCredentials: true,
+  headers: {
+    'UserLang': window.navigator.language,
+    'Content-Type': 'application/json;charset=utf-8',
+  }
+});
+
+export default customizedAxios
+
+// 以下の形でimportすること
+// import {customizedAxios as axios} from '@/tools/customizedAxios.js'
