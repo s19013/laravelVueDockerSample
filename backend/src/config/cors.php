992aec7730fd18c5ba4c3aef173e5de55df92d8a
知念英弥 <s19013@std.it-college.ac.jp>
2024-03-22 15:00:53 +0900

    corsの設定をした

--- backend/src/config/cors.php
+++ backend/src/config/cors.php
@@ -19,7 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    // supports_credentialsがtrueだけどここで許可するサイトを設定してるから安心
+    'allowed_origins' => ['http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
@@ -29,6 +30,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
