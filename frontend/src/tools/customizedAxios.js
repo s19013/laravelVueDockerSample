6fd30110579477c8368d983458f0e74e5287aceb
知念英弥 <s19013@std.it-college.ac.jp>
2024-03-22 19:51:03 +0900

    他の部分がややこしくなるので最後のスラッシュを変えた

--- frontend/src/tools/customizedAxios.js
+++ frontend/src/tools/customizedAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const customizedAxios = axios.create({
-  baseURL: 'http://localhost:8000/api/task/',
+  baseURL: 'http://localhost:8000/api/task',
   withCredentials: true,
   headers: {
     'UserLang': window.navigator.language,
