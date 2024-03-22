9e06c419ba972e5abfa4dac0746c0e0f951c546e
知念英弥 <s19013@std.it-college.ac.jp>
2024-03-22 22:20:41 +0900

    登録画面完成

--- frontend/src/views/Create.vue
+++ frontend/src/views/Create.vue
@@ -1,18 +1,45 @@
-<script setuo>
+<script setup>
 import {ref} from 'vue';
+import {default as axios} from '@/tools/customizedAxios.js'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
 
 
+let task_name = ref('')
+let sending =ref(false)
+let errorMessage = ref('')
+
+const submit = async () => {
+    sending.value=true
+    errorMessage.value = ''
+
+    await axios
+    .post('/',{task_name:task_name.value})
+    .then((response) => {
+        router.push({name:'index'})
+    })
+    .catch((error) => {
+        console.log(error);
+        errorMessage = error.response.data.message
+    });
+
+    sending.value=false
+}
 </script>
 
 <template>
     <div>
+        
         <router-link :to="{name:'index'}">
             <button>戻る</button>
         </router-link>
         <div>
-            <textarea />
-            <button>送信</button>
+            <textarea  placeholder="タスク名" v-model="task_name"/>
+            <button @click="submit">送信</button>
         </div>
+        <p v-if="sending">送信中</p>
+        <p>{{ errorMessage }}</p>
     </div>
 </template>
 
