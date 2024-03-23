08c301d15e52a589c6dea5874952c44be34f7fcb
知念英弥 <s19013@std.it-college.ac.jp>
2024-03-23 14:05:04 +0900

    タスク読み込み

--- frontend/src/views/Index.vue
+++ frontend/src/views/Index.vue
@@ -1,5 +1,22 @@
-<script setuo>
-import {ref} from 'vue';
+<script setup>
+import {ref,reactive,onBeforeMount} from 'vue';
+import Task from '@/components/Task.vue'
+
+import {default as axios} from '@/tools/customizedAxios.js'
+
+let loading = ref(true)
+let tasks = ref(null)
+onBeforeMount(() => {
+    axios
+    .get('/')
+    .then((response) => {
+        tasks = ref(null)
+        tasks.value = response.data
+        loading.value = false
+
+    })
+    .catch((error) => {console.log(error);});
+})
 
 
 </script>
@@ -13,6 +30,13 @@ import {ref} from 'vue';
             <input type="text">
             <button>検索</button>
         </div>
+
+        <p v-if="loading">読込中</p>
+        {{ tasks }}
+        <br>
+        <template v-for="task of tasks" :key="task.id">
+            <Task :task="task"/>
+        </template>
     </div>
 </template>
 
