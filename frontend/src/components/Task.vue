3a4686d2130b762b4f8d85acf22fdb0025782acd
知念英弥 <s19013@std.it-college.ac.jp>
2024-03-23 15:00:06 +0900

    タスクコンポーネント

---
+++ frontend/src/components/Task.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {ref,defineProps} from 'vue';
+
+const props = defineProps({
+    task:{
+        type:Object,
+        default:{
+            id:0,
+            task_name:'',
+            created_date:'',
+        }
+    }
+})
+
+</script>
+
+<template>
+    <div>
+        <h3>{{ task.task_name }}</h3>
+        <p>{{ task.created_date }}</p>
+
+        <router-link :to="'/edit/'+task.id">
+            <button>編集</button>
+        </router-link>
+        <button>完了</button>
+    </div>
+</template>
+
+<style>
+
+</style>
