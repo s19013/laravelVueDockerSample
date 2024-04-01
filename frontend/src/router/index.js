30c2326b9c76238f9ebf057cbc755a7a40d09d30
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-01 22:20:43 +0900

    テストのために色々編集

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,25 +3,27 @@ import Index  from '@/views/Index.vue'
 import Create  from '@/views/Create.vue'
 import Edit  from '@/views/Edit.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: Index
+  },
+  {
+    path: '/create',
+    name: 'create',
+    component: Create
+  },
+  {
+    path: '/edit/:id',
+    name: 'edit',
+    component: Edit
+  },
+]
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL + 'task'),
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: Index
-    },
-    {
-      path: '/create',
-      name: 'create',
-      component: Create
-    },
-    {
-      path: '/edit/:id',
-      name: 'edit',
-      component: Edit
-    },
-  ]
+  routes: routes
 })
-
+export { routes }
 export default router
