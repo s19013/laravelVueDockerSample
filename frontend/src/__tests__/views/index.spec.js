0843376e7d794973eabee25ab31ae6af62cc0773
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-01 22:29:35 +0900

    とりあえず初期状態のテストを作成

--- frontend/src/__tests__/views/index.spec.js
+++ frontend/src/__tests__/views/index.spec.js
@@ -1,64 +1,75 @@
-import { mount } from '@vue/test-utils'
+import { mount,RouterLinkStub } from '@vue/test-utils'
 import Index  from '@/views/Index.vue'
-import {default as axios} from '@/tools/customizedAxios.js'
+import customizedAxios from '@/tools/customizedAxios.js'
 import MockAdapter from 'axios-mock-adapter'
-import {router} from '@/router/index.js'
+import {routes} from '@/router/index.js'
 import { createRouter, createWebHistory } from 'vue-router'
-import { useRoute, useRouter } from 'vue-router';
 
-// const route = useRoute();
-// const router = useRouter();
-
-// const router = createRouter({
-//     history: createWebHistory(),
-//     routes: route
-//   })
-
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes,
+  })
 
 const baseURL = 'http://localhost:8000/api/task'
 
 
-test("初期状態",async () => {
+test("とりあえず初期状態",async () => {
     // axiosを監視して、レスポンスのモックデータを設定
     const url = baseURL + "/"
 
+    const mockedResponse = [
+        {
+            id:1,
+            task_name:'testTask',
+            created_at:'2024-03-23T0000'
+        }
+    ]
 
+    // ライブラリを使った方法ならできた -> ただこのライブラリに依存することになる
+    // axiosをモックする
+    const mockedAxios = new MockAdapter(customizedAxios)
+    mockedAxios.onGet(url).reply(200,mockedResponse)
 
-    const mockedAxios = new MockAdapter(axios)
-    mockedAxios.onGet(url).reply(200,
-        [
-            {
-                id:1,
-                task_name:'testTask',
-                created_at:'2024-03-23T0000'
-            }
-        ]
-    )
+    vi.mock('vue-router', async () => {
+        const route = await vi.importActual('vue-router')
+      
+        return { ...route, 
+            useRoute() {
+                return {
+                  route: "/",
+                  pathname: "",
+                  query: {keyword:""},
+                  asPath: "",
+                };
+              },
+        }
+    })
 
-
-    // vi.mock("vue-router", () => ({
-    //     useRoute() {
-    //       return {
-    //         route: "/",
-    //         pathname: "",
-    //         query: "",
-    //         asPath: "",
-    //       };
-    //     },
-    //   }));
-
+    // なぜこれで帰ってくるのか正直わからない?
+    // axiosがmockされて改変されているから?
     // const response = await axios.get(baseURL + "/")
     // console.log(response.data);
-    // console.log("test");
+    // const response = await customizedAxios.get(baseURL + "/")
+
     // await router.isReady()
     const wrapper = await mount(Index,
         {
             global: {
-              plugins: [router]
-            }
+                plugins: [router],
+                stubs: {RouterLink: RouterLinkStub},
+            },
         }
     )
-    console.log(wrapper.vi.tasks);
 
+    // BeforeMountでやってること再現しないとだめだってさ｡
+    wrapper.vm.keyword = ''
+    await wrapper.vm.search()
+
+
+    // console.log(wrapper.vm.tasks);
+    // console.log(wrapper.vm.route.query); 
+
+    // axiosで通信したデータが変数に入っているか
+    expect(wrapper.vm.tasks).toContainEqual(mockedResponse[0]);
 
 })
