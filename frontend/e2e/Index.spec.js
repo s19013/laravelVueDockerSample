82446720fca091f50e9839495150f702977bef5e
知念英弥 <s19013@std.it-college.ac.jp>
2024-04-06 22:04:15 +0900

    エラーテスト

--- frontend/e2e/Index.spec.js
+++ frontend/e2e/Index.spec.js
@@ -3,7 +3,7 @@ import { test, expect} from '@playwright/test';
 const apiBaseURL = 'http://localhost:8000/api/task'
 
 // propsで使うやつまできっちりとってこような!!
-const mockedResponseJson =[{
+const mockedTasks =[{
     id: 1, 
     task_name: 'test_task_name',
     deleted_at: null,
@@ -11,12 +11,36 @@ const mockedResponseJson =[{
     updated_at: '2024-03-23T07:41:42.000000Z'
 }]
 
-async function mockedResponse(page) {
+const errorMessage = "通信エラーが発生しました｡時間を置いてもう一度送信してください"
+
+async function mockedResponse({page,status,body}) {
     await page.route(apiBaseURL + "/" + "?keyword=", async route => {
         await route.fulfill({
-            status:200,
-            body: JSON.stringify(mockedResponseJson),
+            status:status,
+            body: body
         });
+    });
+}
+
+async function mockedNormalResponse(page) {
+    await mockedResponse({
+        page:page,
+        status:200,
+        body:JSON.stringify(mockedTasks),
+    })
+}
+
+async function mockedServerErrorResponse(page) {
+    await mockedResponse({
+        page:page,
+        status:500,
+        body:JSON.stringify(errorMessage),
+    })
+}
+
+async function mockedNetworkErrorResponse(page) {
+    await page.route(apiBaseURL + "/" + "?keyword=", async route => {
+        await route.abort()
     });
 }
 
@@ -28,12 +52,12 @@ async function clickButton(page,buttonName) {
 // 条件:画面表示
 // 期待:api通信をしてデータを表示
 test('データ表示',async({page}) => {
-    await mockedResponse(page)
+    await mockedNormalResponse(page)
 
     await page.goto('/')
 
-    await expect(page.getByText(mockedResponseJson[0].task_name)).toBeVisible();
-    await expect(page.getByText(mockedResponseJson[0].created_at.split("T")[0])).toBeVisible();
+    await expect(page.getByText(mockedTasks[0].task_name)).toBeVisible();
+    await expect(page.getByText(mockedTasks[0].created_at.split("T")[0])).toBeVisible();
 })
 
 // 条件:inputに文字列を入れて送信
@@ -42,7 +66,7 @@ test('検索', async ({ page }) => {
     await page.goto('/');
     const input = await page.getByRole('textbox')
     await input.fill('test');
-    
+
     await clickButton(page,"検索")
 
      // URLにクエリパラメータが含まれているかを確認
@@ -67,7 +91,7 @@ test.describe('画面遷移', () => {
     // 条件:編集ボタンを押す
     // 期待:編集画面へ遷移
     test('編集', async ({page}) => { 
-        await mockedResponse(page)
+        await mockedNormalResponse(page)
 
         await page.goto('/')
 
@@ -79,4 +103,20 @@ test.describe('画面遷移', () => {
     })
  })
 
-// test('エラー', () => { second })
+test.describe('エラー', () => { 
+    // 条件:api通信時に500エラーが帰ってくる
+    // 期待:エラー文を表示
+    test('サーバーエラー', async({page}) => { 
+        await mockedServerErrorResponse(page)
+        await page.goto('/')
+        await expect(page.getByText(errorMessage)).toBeVisible();
+     })
+
+    // 条件:api通信時にネットワークエラーが起きる
+    // 期待:エラー文を表示
+    test('ネットワークエラー', async({page}) => { 
+        await mockedNetworkErrorResponse(page)
+        await page.goto('/')
+        await expect(page.getByText(errorMessage)).toBeVisible();
+     })
+ })
